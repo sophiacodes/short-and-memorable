2d67330e8758a6dcadf74422569d93d049529130
sophiacodes <sophiacodes@gmail.com>
2018-08-12 14:51:22 +0100

    Changed app logo and updated screenshot

--- client/src/react-components/logo/index.jsx
+++ client/src/react-components/logo/index.jsx
@@ -5,10 +5,10 @@ const Logo = () => {
     return (
         <Fragment>
           <h1 className="title logo">
-            <span className="logo-left">Open</span>Law 
+            <span className="logo-left">Short&amp;</span>Memorable.
           </h1>
           <h2 className="subtitle">
-            Next generation <b>smart</b> legal agreements.
+            Word.
           </h2>
         </Fragment>
     )
